de1f5a55905dd88cd39eebb1e76ee1db1f3b0cb3
CAFUCH <tjfdk6201@gmail.com>
2023-08-17 19:04:07 +0900

    Shop_UI Setting
    horizontal & vertical ScrollView

---
+++ Cat that repays kindness/Assets/01.Scripts/ItemSO.cs
@@ -0,0 +1,16 @@
+using System.Net.Mime;
+using System.Runtime.InteropServices;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemSO : MonoBehaviour
+{
+    private int price;
+    private string name;
+    private Image image;
+    // private super super;
+
+    
+}
